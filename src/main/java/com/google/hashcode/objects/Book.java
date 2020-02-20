8686b4e5f81c5c2ff313df04b144037c47ba2087
Dmitry Mikryukov <dmikryukov@natera.com>
2020-02-20 21:28:45 +0300

    Data model

---
+++ src/main/java/com/google/hashcode/objects/Book.java
@@ -0,0 +1,6 @@
+package com.google.hashcode.objects;
+
+public class Book {
+    public Integer id;
+    public Integer score;
+}
---
+++ src/main/java/com/google/hashcode/objects/Library.java
@@ -0,0 +1,9 @@
+package com.google.hashcode.objects;
+
+import java.util.List;
+
+public class Library {
+    public Integer id;
+    public Integer signupTime;
+    public List<Book> books;
+}
