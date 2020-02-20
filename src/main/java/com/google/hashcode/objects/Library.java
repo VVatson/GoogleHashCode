3cea74a709611c564fcfb9d02a08dcb8b3ba94ab
Dmitry Mikryukov <dmikryukov@natera.com>
2020-02-20 22:26:52 +0300

    Update library

--- src/main/java/com/google/hashcode/objects/Library.java
+++ src/main/java/com/google/hashcode/objects/Library.java
@@ -4,5 +4,13 @@ import java.util.List;
 public class Library {
     public Integer id;
     public Integer signupTime;
+    public Integer sendCount;
     public List<Book> books;
+
+    public Library(Integer id, Integer signupTime, Integer sendCount, List<Book> books) {
+        this.id = id;
+        this.signupTime = signupTime;
+        this.sendCount = sendCount;
+        this.books = books;
+    }
 }
