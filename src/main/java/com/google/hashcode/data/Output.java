f4c7a00a3ce0e0417f2f6e9068e2af25afdb39c8
Dmitry Mikryukov <dmikryukov@natera.com>
2020-02-20 21:56:00 +0300

    Output data model + tests

--- src/main/java/com/google/hashcode/data/Output.java
+++ src/main/java/com/google/hashcode/data/Output.java
@@ -1,51 +1,21 @@
 package com.google.hashcode.data;
 
-import com.google.hashcode.objects.Vehicle;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.google.hashcode.objects.Book;
+import com.google.hashcode.objects.Library;
 
 /**
  * Output data that contains a schedule of the rides.
  */
 public class Output {
+    public final LibrarySet[] librarySets;
 
-    private List<List<Integer>> schedule;
-
-    public Output(final List<List<Integer>> schedule) {
-        this.schedule = schedule;
+    public Output(LibrarySet[] librarySets) {
+        this.librarySets = librarySets;
     }
 
-    public static Output fromVehicles(List<Vehicle> vehicles) {
-        List<List<Integer>> schedule = new ArrayList<>();
-        for (Vehicle vehicle : vehicles) {
-            schedule.add(vehicle.getCompletedDrives().stream()
-                    .sequential()
-                    .map(drive -> drive.getNumberDrive())
-                    .collect(Collectors.toList()));
-        }
-        return new Output(schedule);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        for (List<Integer> rides : schedule) {
-            if (rides != null && !rides.isEmpty())
-            sb.append(rides.size());
-            sb.append(" ");
-
-            String ridesGapSeparated = rides.stream()
-                    .sequential()
-                    .map(String::valueOf)
-                    .collect(Collectors.joining(" "));
-            sb.append(ridesGapSeparated);
-            sb.append("\n");
-
-        }
-        return sb.toString();
+    public static class LibrarySet {
+        public Library library;
+        public Book[] books;
     }
 }
 
---
+++ src/test/java/com/google/hashcode/Test.java
@@ -0,0 +1,9 @@
+package com.google.hashcode;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void test() {
+        System.out.println("test");
+    }
+}
