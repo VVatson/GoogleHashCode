65ed356a9a2cfb8c7ad569c572f1917bdfefcfc5
Vasily Kumaev <vkumaev@natera.com>
2020-02-20 22:19:47 +0300

    Lib com

---
+++ src/main/java/com/google/hashcode/objects/LibraryState.java
@@ -0,0 +1,15 @@
+package com.google.hashcode.objects;
+
+/**
+ * State of the library.
+ */
+public enum LibraryState {
+
+    WAITING_FOR_SIGNUP,
+
+    SIGNING_UP,
+
+    SHIPPING,
+
+    FINISHED
+}
